fde1b7e3aa6c2bebb66161a52a600fac602804e5
Errant <48056361+errantSquam@users.noreply.github.com>
2025-12-02 17:05:24 +0800

    part 2 solved

--- day2/solution.cpp
+++ day2/solution.cpp
@@ -20,6 +20,98 @@ vector<string> split_string(string input_string, char delimiter)
     return seglist;
 }
 
+bool check_invalid_id_2(string id) {
+    for (int window_len = 1; window_len < id.length(); window_len++) {
+        
+
+        if (id.length() % window_len != 0) {
+            continue;
+        }
+
+        string substring = id.substr(0, window_len);
+
+        int repetitions = 1;
+
+        bool isRepeated = true;
+
+        while (window_len * (repetitions + 1) <= id.length()) {
+            int start_index = window_len * (repetitions);
+            string temp_substring = id.substr(start_index, window_len);
+
+            if (temp_substring != substring) {
+                isRepeated = false;
+                break;
+            }
+            repetitions++;
+        }
+        if (isRepeated) {
+            return true;
+        }
+
+    }
+    return false;
+}
+
+void get_invalid_id_2(string input_file)
+{
+
+    ifstream infile(input_file);
+    stringstream buffer;
+
+    buffer << infile.rdbuf();
+    string line = buffer.str();
+
+    infile.close();
+
+    stringstream id_ranges(line);
+    string id_range;
+
+    vector<long long> invalid_id_vector;
+
+    while (getline(id_ranges, id_range, ','))
+    {
+        vector<string> ids = split_string(id_range, '-');
+        string first_id = ids.front();
+        string last_id = ids.back();
+
+        cout << "Last ID: " + last_id << '\n';
+
+        vector<long long> invalid_ids;
+
+        for (long long id = stoll(first_id); id <= stoll(last_id); id++) {
+            string id_string = to_string(id);
+
+            if (check_invalid_id_2(id_string)) {
+                invalid_ids.push_back(id);
+            }
+        }
+
+        if (invalid_ids.size() > 0) {
+            cout << id_range + " has " +
+            to_string(invalid_ids.size()) + 
+            " invalid IDs, ";
+            for (long long i = 0; i < invalid_ids.size(); i++) {
+                if (i > 0) {
+                    cout << ", ";
+                }
+                cout << invalid_ids[i];
+            }
+            cout << '\n';
+        }
+
+        invalid_id_vector.insert(invalid_id_vector.end(),
+        invalid_ids.begin(), invalid_ids.end());
+    }
+    cout << "The rest of the ranges contain no invalid IDs." << '\n';
+
+    long long sum_ids = accumulate(invalid_id_vector.begin(),
+    invalid_id_vector.end(), 0LL);
+
+    cout << "Adding up all the invalid IDs produces "
+    + to_string(sum_ids) << '\n';
+
+}
+
 void get_invalid_id_1(string input_file)
 {
 
@@ -38,7 +130,6 @@ void get_invalid_id_1(string input_file)
 
     while (getline(id_ranges, id_range, ','))
     {
-        //cout << id_range << '\n';
         vector<string> ids = split_string(id_range, '-');
         string first_id = ids.front();
         string last_id = ids.back();
@@ -49,9 +140,6 @@ void get_invalid_id_1(string input_file)
 
         for (long long id = stoll(first_id); id <= stoll(last_id); id++) {
             string id_string = to_string(id);
-            /*cout << "ID: " + id_string 
-            + " ID string length: " 
-            + to_string(id_string.length()) << '\n';*/
 
             if (id_string.substr(0, id_string.length()/2)
             .compare(id_string.substr(id_string.length()/2, 
@@ -93,5 +181,8 @@ int main()
     string test_input = "test_input.txt";
 
     //get_invalid_id_1(sample_input);
-    get_invalid_id_1(input);
+    //get_invalid_id_1(input);
+
+    //get_invalid_id_2(sample_input);
+    get_invalid_id_2(input);
 }
