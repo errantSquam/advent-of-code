cdd7ff2c27613d3fc45e1138e4921819811b8932
Errant <48056361+errantSquam@users.noreply.github.com>
2025-12-06 20:08:36 +0800

    day 6 finally done

--- day6/solution.py
+++ day6/solution.py
@@ -1,6 +1,10 @@
 from functools import reduce
 import os 
 import numpy as np
+
+import re
+
+
 input_file = open(os.getcwd() + '/input/input.txt', 'r').read().split('\n')
 sample_input_file = open(os.getcwd() + '/input/sample_input.txt', 'r').read().split('\n')
 
@@ -8,6 +12,9 @@ sample_input_file = open(os.getcwd() + '/input/sample_input.txt', 'r').read().sp
 def parse_problem(input):
     input = [[j for j in i.split(' ') if j != ''] for i in input]
     return input
+
+def parse_operator_index(operatorList):
+    return [m.start() for m in re.finditer(r'\*|\+', operatorList)]
 
 def solve_problem_1(input):
     input = parse_problem(input)
@@ -35,7 +42,7 @@ def solve_problem_1(input):
     print(solutions)
     print(f"The total sum is {sum(solutions)}.")
             
-def solve_problem_2(input):
+def solve_problem_2_old(input):
     input = parse_problem(input)
     operatorList = input[-1]
     matrices = []
@@ -91,5 +98,46 @@ def solve_problem_2(input):
     print(f"Solutions are: {solutions}")
     return 
 
-solve_problem_2(sample_input_file)
-#solve_problem_1(input_file)
+def solve_problem_2(input):
+    operatorList = input[-1]
+    input = [row + ' ' for row in input]
+    operatorIndices = parse_operator_index(operatorList)
+    parts = [[row[i:j] for i,j in zip(operatorIndices, operatorIndices[1:]+[None])] for row in input]
+    parts = parts[0:len(parts)-1]
+    parts = [[j[0:len(j)-1] for j in i] for i in parts]
+
+
+    parts = np.array(parts).T.tolist()
+
+    operatorList = [i for i in input[-1].split(" ") if i != ""]
+
+    solutions = []
+
+    for part_index in range(len(parts)):
+        part = parts[part_index]
+        operator = operatorList[part_index]
+        newNums = ['' for i in range(len(part[0]))]
+        for number in part:
+            for digit in range(len(number)):
+                #print(digit)
+                currentDigit = number[digit]
+                #print(f"Current Digit: {currentDigit}")
+                if currentDigit != ' ':
+                    newNums[digit] += currentDigit
+
+        newNums = [int(i) for i in newNums]
+
+        solution = 0
+        if operator == "*":
+            solution = reduce(lambda x,y: x*y, newNums)
+            print(f"{newNums} multiply to form {solution}")
+        elif operator == "+":
+            solution = sum(newNums)        
+            print(f"{newNums} add to form {solution}")
+
+        solutions.append(solution)
+                
+    print(f"The final sum is {sum(solutions)}.")
+
+#solve_problem_2(sample_input_file)
+solve_problem_2(input_file)
